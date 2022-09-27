88018cd7e92bb8cb8765a6c2ac27fb7c99254873
chang <ckdtns5262@nate.com>
2022-09-28 00:11:36 +0900

    [09.27] map함수

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# React-
+# React
+ + [09.26] : component 및 modal창 만들기 
+ + [09.27] : map 함수 및 props 개념
--- src/App.js
+++ src/App.js
@@ -2,60 +2,86 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  
- 
-  const [글제목,글제목변경]= useState(['부산 맛집 추천', '강남 맛집 추천', '리액트독학'])
-  const [따봉,따봉변경]= useState(0);
-  const [modal, showModal] =useState(false);
 
- 
-  return ( 
+
+  const [글제목, 글제목변경] = useState(['부산 맛집 추천', '강남 맛집 추천', '리액트독학'])
+  const [따봉, 따봉변경] = useState([0,0,0]);
+  const [modal, showModal] = useState(false);
+  
+
+  return (
     <div className="App">
       <div className="black-nav">
         <h4>블로그임</h4>
       </div>
-      <button onClick={()=>{
-        const copy =[...글제목];
+      <button onClick={() => {
+        const copy = [...글제목];
         copy.sort();
-         글제목변경(copy)}}>가나다순정렬</button>
-      <button onClick={()=>{
+        글제목변경(copy)
+      }}>가나다순정렬</button>
+      <button onClick={() => {
         const copy = [...글제목];    // ... 괄호를뺴주세요
         copy[0] = "여자 코트 추천";
-          글제목변경(copy);
-          }}>글제목수정</button>
+        글제목변경(copy);
+      }}>글제목수정</button>
+
+
+      {/* <div className='list'>
+        <h4>{글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }}>👍🏿</span>
+          {따봉}</h4>
+        <p>9월 24일 발행</p>
+      </div>
 
       <div className='list'>
-        <h4>{ 글제목[0] } <span onClick={()=>{따봉변경(따봉+1)}}>👍🏿</span>
-        {따봉}</h4>
-        <p>9월 24일 발행</p>
-      </div>
-      
-      <div className='list'>
-        <h4>{ 글제목[1] }</h4>
+        <h4>{글제목[1]}</h4>
         <p>9월 24일 발행</p>
       </div>
       <div className='list'>
-        <h4 onClick={()=>{showModal(!modal)}}>{ 글제목[2] }</h4>
+        <h4 onClick={() => { showModal(!modal) }}>{글제목[2]}</h4>
         <p>9월 24일 발행</p>
-      </div>
-     {
-      // 조건식 ? 참일 때 실행할 코드 : 거짓일 때 실행할 코드
-      // 1 == 1 ? '맞음' : '아님' => 삼항 연산자
-      modal == true ? <Modal/> : null
+      </div> */}
       
-     }
+      {
+        글제목.map(function(a, i){
+          return ( 
+          <div className='list'>
+          <h4 onClick={() => { showModal(!modal) }} >{글제목[i]}
+          <span onClick={() => { 
+            let copy = [...따봉];
+            copy[i] = copy[i]+1;
+            따봉변경(copy)
+            }}>👍🏿</span>
+          {따봉[i]}</h4>
+          <p>9월 24일 발행</p>
+        </div>
+
+          )
+        })
+      }
+
+      {
+        // 조건식 ? 참일 때 실행할 코드 : 거짓일 때 실행할 코드
+        // 1 == 1 ? '맞음' : '아님' => 삼항 연산자
+        modal == true ? <Modal 글제목변경={글제목변경}  color={'skyblue'}글제목 = {글제목}/> : null
+
+      }
     </div>
   );
 
-      function Modal(){
-        return(
-          <div className='modal'>
-          <h4>제목</h4>
-          <h4>날짜</h4>
-          <h4>상세내용</h4>
-        </div>
-        )
-      }
+  function Modal(props) {
+    return (
+      <div className='modal' style={{background : props.color}}>
+        <h4>{props.글제목[0]}</h4>
+        <h4>날짜</h4>
+        <h4>상세내용</h4>
+        <button onClick={()=>{
+          const 변경 =[...글제목]
+          변경[0] = "여자코트추천"
+          글제목변경(변경);
+        }}>글수정</button>
+      </div>
+    )
+  }
 
 }
 
